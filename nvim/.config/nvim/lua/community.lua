b7264c79e92f8d2c843dc9d14f0207d3f0755d87
chongyan <chongyanx@163.com>
2024-12-20 13:51:07 +0800

    feat: Add initial AstroNvim config and setup files

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,14 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.completion.cmp-cmdline" },
+  { import = "astrocommunity.recipes.neovide" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+}
---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    optional = true,
+    opts = {
+        presets = {
+            bottom_search = false,
+            inc_rename = true
+        },
+    }
+}
